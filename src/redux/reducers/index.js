5f6578eff0f6c3567195296ca6f0a47aef727ee2
BURAK SONMEZ <42543196+xbsonmez@users.noreply.github.com>
2022-03-27 20:57:29 +0300

    Updates

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import taskReducer from './taskReducer';
-
-const rootReducer = combineReducers({ 
-    allTask: taskReducer,
-});
-
-export default rootReducer;
-
--- src/redux/sagas/index.js
+++
@@ -1,8 +0,0 @@
-import {all} from 'redux-saga/effects';
-import taskSaga  from './taskSaga';
-
-export default function* rootSaga() {
-    yield all([
-        taskSaga(),
-    ])
-}
